55b49a1c4ee8ca2cd4f4605989b36af043373f48
AlejandroEsteveMaza <alex.esteve98@gmail.com>
2022-11-15 15:23:21 +0100

    Pasar datos a la vista

--- src/main/java/com/springboot/web/app/controler/IndexController.java
+++ src/main/java/com/springboot/web/app/controler/IndexController.java
@@ -1,12 +1,14 @@
 package com.springboot.web.app.controler;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
  @GetMapping({"/","/index","/home"})
-	public String index() {
+	public String index(Model model) {
+	 model.addAttribute("titulo", "Hola Mundo!");
 		return "index";
 	}
 	
